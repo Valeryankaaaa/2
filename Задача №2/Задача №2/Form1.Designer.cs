ec287526cb0e8cad2c603e2711064b1d40107f22
Valeryankaaaa <105592068+Valeryankaaaa@users.noreply.github.com>
2022-05-20 19:09:20 +0500

    Add files via upload

---
+++ Задача №2/Задача №2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Задача__2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите количество судей:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите количество фигуристов:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(306, 35);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(306, 70);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 113);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Получить таблицу оценок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 214);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(474, 168);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(282, 213);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 405);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Задача №2/Задача №2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задача__2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text); //переменная,предназначенная для считывания количества судей
+            if (n < 3 || n > 10) //условие,предназначенное для выполнения условия задачи с количеством судей
+            {
+                MessageBox.Show("Вы ввели неверное значение. Повторите попытку!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); //при выполнении условия будет появляться окно с ошибкой
+                n = 0; //обнуляем кол-во судей
+            }
+            int m = Convert.ToInt32(textBox2.Text); //переменная,предназначенная для считывания количества фигуристов
+            double sum = 0; //переменная,предназначенная для накопления суммы
+            double sr = 0; //переменная,предназначенная для вычисления среднего значения оценки
+            double[,] mas = new double[m, n]; //создаём массив
+            Random rnd = new Random(); //объявляем переменную для рандома чисел
+            dataGridView1.RowCount = m; //присваиваем строкам таблицы фигуристов
+            dataGridView1.ColumnCount = n; //присваиваем столбцам таблицы судей
+            for (int i = 0; i < m; i++) //запускаем цикл для заполнения массива
+            {
+                for (int j = 0; j < n; j++) //запускаем цикл для заполнения массива
+                {
+                    mas[i, j] = Convert.ToDouble(rnd.Next(0, 100) / 10.0); //задаём массиву рандомные дробные значения
+                    dataGridView1.Rows[i].Cells[j].Value = mas[i, j]; //выводим наши значения в таблицу
+                }
+            }
+            for (int i = 0; i < n; i++) //запускаем цикл по количеству столбцов
+            {
+                dataGridView1.Columns[i].HeaderText = Convert.ToString(i + 1) + " судья"; //добавляем название каждого столбца таблицы
+            }
+            for (int i = 0; (i <= (dataGridView1.Rows.Count - 1)); i++) //запускаем цикл по количеству строк таблицы
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0"); //записываем в каждой строке номер фигуриста
+            }
+            for (int i = 0; i < m; i++) //запускаем цикл для вычисления средней оценки
+            {
+                double max = 0; //переменная,предназначенная для выявления максимальной оценки
+                double min = mas[0, 0]; //переменная,предназначенная для выявления минимальной оценки
+                for (int j = 0; j < n; j++) //запускаем цикл для вычисления средней оценки
+                {
+                    sum += mas[i, j]; //суммируем элементы массива
+                    if (max < mas[i, j]) //если начальное значение переменной меньше элемента массива,то…
+                    {
+                        max = mas[i, j]; //переменной присваивается этот элемент массива
+                    }
+                    if (min > mas[i, j]) //если начальное значение переменной больше элемента массива,то…
+                    {
+                        min = mas[i, j]; //переменной присваивается этот элемент массива
+                    }
+                }
+                sr = (sum - min - max) / (n - 2); //высчитываем среднее значение,при условии,что откидывается одна максимальная и одна минимальная оценка судей у каждого фигуриста
+                listBox1.Items.Add("Итоговая оценка " + (i + 1) + " фигуриста: " + Math.Round(sr, 1)); //выводим итоговую оценку каждого фигуриста на экран
+                sum = 0; //обнуляем сумму для дальнейших вычислений
+            }
+        } 
+    }
+}
